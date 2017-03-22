bbb8df80f6c55c9e1b37c58632f2242e2202304a
tss <tss@debian-tss24>
2017-03-22 08:39:53 +0100

    t

---
+++ public_html/21-03/es1/dati/testo1.js
@@ -0,0 +1,18 @@
+a = [{
+    "cognome": "Iaffaldano",
+    "nome": "Lisa",
+    "email": "lisa.iaffa@gmail.com",
+    "foto": "l.jpg"
+},
+{
+    "cognome": "Park",
+    "nome": "Yoochun",
+    "email": "park.iaffa@gmail.com",
+    "foto": "y.jpg"
+},
+{
+    "cognome": "Kim",
+    "nome": "Junsu",
+    "email": "junsu.kim.com",
+    "foto": "j.jpg"
+}];
